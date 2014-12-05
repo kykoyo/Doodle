c552e7954b0c31bd9d7813d9f991012d477479b6
kykoyo <kykoyo1992@gmail.com>
2014-12-04 18:34:46 -0800

    added controllers and views

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,38 @@
 class UserController < ApplicationController
+	before_action :find_params, only[:show, :edit, :update]
+
+	def find_params
+		@user =User.find(params[:id])
+	end
+
 	def index
 		@users=User.all
 	end
+
+	def show
+	end
+
+	def new
+		@user=User.new
+	end
+	
+	def create
+		@user=User.new(user_params)
+	end
+	
+	def edit
+		@user=User.find(params[:id])
+	end
+
+	def update
+		@user.update(user_params)
+	end
+
+	def destroy
+		@user.destroy
+		redrect_to projects_path
+	end
+
+
+
 end
